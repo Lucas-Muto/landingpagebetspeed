5558f22dd34b4252466f1bbe27583e5c91f335e3
unknown <lucasmuto@id.uff.br>
2024-10-21 05:36:33 -0300

    teste2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,6 +13,7 @@ import raio2 from '@/components/imagens/raio2.svg';
 import novo from '@/components/imagens/novo.svg';
 import novo2 from '@/components/imagens/novo2.svg';
 
+
 export default function Login() {
   const [username, setUsername] = useState('');
   const router = useRouter();
@@ -39,129 +40,114 @@ export default function Login() {
   };
 
   return (
-    <>
-      {/* Estilos específicos para telas menores ou iguais a 430px */}
-      <style jsx global>{`
-        @media (max-width: 430px) {
-          body {
-            /* Removido height fixo e overflow hidden para permitir a rolagem */
-            height: auto;
-            overflow: auto;
-          }
-          #main-container {
-            /* Removido height fixo e overflow hidden para permitir a rolagem */
-            height: auto;
-            overflow: auto;
-          }
-        }
-      `}</style>
+    <div className="flex h-screen min-h-screen overflow-y-scroll overflow-x-hidden items-center justify-center bg-black font-poppins">
 
-      <div
-        id="main-container"
-        className="flex min-h-screen overflow-auto items-center justify-center bg-black font-poppins touch-auto"
-      >
+      {/* Plano de fundo como uma imagem */}
+      <div className="fixed top-0 left-0 w-full h-full z-0">
+        <Image
+          src={fundo}
+          alt="Plano de fundo"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-        {/* Plano de fundo como uma imagem */}
-        <div className="fixed top-0 left-0 w-full h-full z-0">
-          <Image
-            src={fundo}
-            alt="Plano de fundo"
-            className="w-full h-full object-cover"
-          />
-        </div>
+      {/* Container para a imagem cinza */}
+      <div className="relative z-10 justify-center xl:justify-start ml-[700px] mb-[90px] sm:flex">
+        <Image
+          src={cartao}
+          alt="Imagem cinza"
+          width={691}
+          height={639}
+          className="object-cover rounded-[15px]"
+        />
+      </div>
 
-        {/* Container para a imagem cinza */}
-        <div className="relative z-10 justify-center xl:justify-start ml-[700px] mb-[90px] sm:flex">
-          <Image
-            src={cartao}
-            alt="Imagem cinza"
-            width={691}
-            height={639}
-            className="object-cover rounded-[15px]"
-          />
-        </div>
+      {/* Posiciona a logo no canto superior esquerdo */}
+      <div className="absolute smaller:relative smaller:flex smaller:justify-center smaller:mt-[-800px] top-[15px] left-[250px] smaller:translate-x-[-260px] p-4 z-10">
+        <Image
+          src={betspeed}
+          alt="Logo betspeed"
+          width={237} // Largura ajustada
+          height={77} // Altura ajustada
+          className="w-[237px] h-[77px]"
+        />
+      </div>
 
-        {/* Posiciona a logo no canto superior esquerdo */}
-        <div className="absolute smaller:relative smaller:flex smaller:justify-center smaller:mt-[-800px] top-[15px] left-[250px] smaller:translate-x-[-260px] p-4 z-10">
-          <Image
-            src={betspeed}
-            alt="Logo betspeed"
-            width={237} // Largura ajustada
-            height={77} // Altura ajustada
-            className="w-[237px] h-[77px]"
-          />
-        </div>
+      <div className="smaller:block hidden mr-[70px] mt-[-780px] absolute text-center z-20">
+        {/* Imagem "parabens" substituindo os textos */}
+        <Image
+          src={novo2}
+          alt="Parabéns"
+          width={202} // Ajuste a largura conforme necessário
+          height={20} // Ajuste a altura conforme necessário
+          className="mx-auto mb-6"
+        />
+      </div>
 
-        <div className="smaller:block hidden mr-[35px] mt-[-780px] absolute text-center z-20">
-          {/* Imagem "parabens" substituindo os textos */}
-          <Image
-            src={novo2}
-            alt="Parabéns"
-            width={202} // Ajuste a largura conforme necessário
-            height={66} // Ajuste a altura conforme necessário
-            className="mx-auto mb-6"
-          />
-        </div>
+   
+      <div className="smaller:block hidden mr-[70px] mt-[-300px] absolute text-center z-20">
+        {/* Imagem "parabens" substituindo os textos */}
+        <Image
+          src={novo}
+          alt="Parabéns"
+          width={389} // Ajuste a largura conforme necessário
+          height={253} // Ajuste a altura conforme necessário
+          className="mx-auto mb-6"
+        />
+      </div>
 
-        <div className="smaller:block hidden mr-[70px] mt-[-350px] absolute text-center z-20">
-          {/* Imagem "parabens" substituindo os textos */}
-          <Image
-            src={novo}
-            alt="Parabéns"
-            width={389} // Ajuste a largura conforme necessário
-            height={253} // Ajuste a altura conforme necessário
-            className="mx-auto mb-6"
-          />
-        </div>
+      <div className="smaller:block hidden mr-6 mt-[230px] absolute text-center z-20">
+        {/* Imagem "parabens" substituindo os textos */}
+        <Image
+          src={raio2}
+          alt="Parabéns"
+          width={329} // Ajuste a largura conforme necessário
+          height={132} // Ajuste a altura conforme necessário
+          className="mx-auto mb-6"
+        />
+      </div>
 
-        <div className="smaller:block hidden mr-6 mt-[200px] absolute text-center z-20">
-          {/* Imagem "parabens" substituindo os textos */}
-          <Image
-            src={raio2}
-            alt="Parabéns"
-            width={329} // Ajuste a largura conforme necessário
-            height={132} // Ajuste a altura conforme necessário
-            className="mx-auto mb-6"
-          />
-        </div>
 
-        <div className="absolute text-center z-20">
-          {/* Imagem "parabens" substituindo os textos */}
-          <Image
-            src={parabens}
-            alt="Parabéns"
-            width={537} // Ajuste a largura conforme necessário
-            height={163} // Ajuste a altura conforme necessário
-            className="mx-auto mb-6 smaller:hidden mr-[655px]"
-          />
+      <div className="absolute text-center z-20">
+        {/* Imagem "parabens" substituindo os textos */}
+        <Image
+          src={parabens}
+          alt="Parabéns"
+          width={537} // Ajuste a largura conforme necessário
+          height={163} // Ajuste a altura conforme necessário
+          className="mx-auto mb-6 smaller:hidden mr-[655px]"
+        />
 
-          {/* Container para centralizar o input e o botão */}
-          <div className="flex flex-col items-center mr-[690px] mt-[50px] smaller:translate-x-[333px] smaller:mt-[550px]">
-            {/* Campo de Input */}
-            <div className="flex items-center border border-gray-300 rounded-[10px] bg-white px-3 py-2 mb-4 w-[458px] smaller:w-[340px] h-[64px]">
-              {/* Ícone de usuário */}
-              <UserIcon className="h-5 w-5 text-gray-400 mr-2" />
-              {/* Input com estilo Tailwind e evento onKeyDown */}
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Nome"
-                className="outline-none bg-transparent w-full text-gray-700 placeholder-gray-400 border-none"
-                onKeyDown={handleKeyPress} // Adicionado evento para detectar "ENTER"
-              />
-            </div>
 
-            {/* Botão de Validar */}
-            <button
-              onClick={handleLogin}
-              className="flex items-center justify-center bg-[#E5C680] text-[#0C0E0D] font-semibold py-2 px-4 rounded-[10px] hover:scale-105 transition-transform w-[158px] h-[60px] mt-2"
-            >
-              Acessar
-            </button>
+
+        {/* Container para centralizar o input e o botão */}
+        <div className="flex flex-col items-center mr-[690px] mt-[50px] smaller:translate-x-[333px] smaller:mt-[550px]">
+          {/* Campo de Input */}
+          <div className="flex items-center border border-gray-300 rounded-[10px] bg-white px-3 py-2 mb-4 w-[458px] smaller:w-[340px] h-[64px]">
+            {/* Ícone de usuário */}
+            <UserIcon className="h-5 w-5 text-gray-400 mr-2" />
+            {/* Input com estilo Tailwind e evento onKeyDown */}
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Nome"
+              className="outline-none bg-transparent w-full text-gray-700 placeholder-gray-400 border-none"
+              onKeyDown={handleKeyPress} // Adicionado evento para detectar "ENTER"
+            />
           </div>
+
+
+
+          {/* Botão de Validar */}
+          <button
+            onClick={handleLogin}
+            className="flex items-center justify-center bg-[#E5C680] text-[#0C0E0D] font-semibold py-2 px-4 rounded-[10px] hover:scale-105 transition-transform w-[158px] h-[60px] mt-2"
+          >
+            Acessar
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
